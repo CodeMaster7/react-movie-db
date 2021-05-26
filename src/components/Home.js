16b03fe060e744d68999ac2f80a986af14c9893f
sam <sammybonfanti@gmail.com>
2021-05-26 10:31:46 -0700

    getting the movie id

--- src/components/Home.js
+++ src/components/Home.js
@@ -49,29 +49,27 @@ const Home = () => {
 
     return (
 		<>
-            {!searchTerm && (
-                <HeroImage
-                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
-                    title={heroImage.original_title}
-                    text={heroImage.overview}
-                />
-            )}
+			{!searchTerm && (
+				<HeroImage
+					image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+					title={heroImage.original_title}
+					text={heroImage.overview}
+				/>
+			)}
 			<SearchBar callback={searchMovies} />
 			<Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
-                {movies.map(movie => (
-                    <MovieThumb
-                        key={movie.id}
-                        clickable
-                        image={movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}` : NoImage}
-                        movie={movie.id}
-                        movieName={movie.original_title}
-                    />
-                ))}
-            </Grid>
+				{movies.map((movie) => (
+					<MovieThumb
+						key={movie.id}
+						clickable
+						image={movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}` : NoImage}
+						movieId={movie.id}
+						movieName={movie.original_title}
+					/>
+				))}
+			</Grid>
 			{loading && <Spinner />}
-            {currentPage < totalPages && !loading && (
-			<LoadMoreBtn text="Load More" callback={loadMoreMovies} />
-            )}
+			{currentPage < totalPages && !loading && <LoadMoreBtn text='Load More' callback={loadMoreMovies} />}
 		</>
 	)
 }
--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from '@reach/router'
 
 import RMDBLogo from '../images/reactMovie_logo2.png'
 import TMDBLogo from '../images/tmdb_logo.svg'
@@ -12,8 +13,10 @@ import {
 const Header = () => (
 	<StyledHeader>
 		<div className='header-content'>
-			<StyledRMDBLogo src={RMDBLogo} alt='rmdb-logo' />
-			<StyledTMDBLogo src={TMDBLogo} alt='tmdb-logo' />
+            <Link to="/">
+                <StyledRMDBLogo src={RMDBLogo} alt='rmdb-logo' />
+            </Link>
+            <StyledTMDBLogo src={TMDBLogo} alt='tmdb-logo' />
 		</div>
 	</StyledHeader>
 )
