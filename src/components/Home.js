bda36d6e36ea9eec8e894f7c1a3296e3a310ba07
sam <sammybonfanti@gmail.com>
2021-05-26 09:14:52 -0700

    load more button working and movies showing up

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import {
     API_URL,
     API_KEY,
@@ -18,24 +18,51 @@ import Spinner from "./elements/Spinner";
 // Custom Hook
 import { useHomeFetch } from './hooks/useHomeFetch'
 
+import NoImage from './images/no_image.jpg'
+
 const Home = () => {
-    const [{ state, loading, error }, fetchMovies] = useHomeFetch()
+    const [
+        {
+            state: { movies, currentPage, totalPages, heroImage },
+            loading,
+            error
+        }, fetchMovies] = useHomeFetch()
+    const [searchTerm, setSearchTerm ] = useState('')
+
+    const loadMoreMovies = () => {
+        const searchEndpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage + 1}`
+        const popularEndPoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${currentPage + 1}`
+
+        const endpoint = searchTerm ? searchEndpoint : popularEndPoint
+        fetchMovies(endpoint)
+    }
 
     if (error) return <div>Something went wrong ...</div>
-	if (!state.movies[0]) return <Spinner />
+	if (!movies[0]) return <Spinner />
 
     return (
 		<>
 			<HeroImage
-				image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-				title={state.heroImage.original_title}
-				text={state.heroImage.overview}
+				image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+				title={heroImage.original_title}
+				text={heroImage.overview}
 			/>
 			<SearchBar />
-			<Grid />
-			<MovieThumb />
-			<Spinner />
-			<LoadMoreBtn />
+			<Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+                {movies.map(movie => (
+                    <MovieThumb
+                        key={movie.id}
+                        clickable
+                        image={movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}` : NoImage}
+                        movie={movie.id}
+                        movieName={movie.original_title}
+                    />
+                ))}
+            </Grid>
+			{loading && <Spinner />}
+            {currentPage < totalPages && !loading && (
+			<LoadMoreBtn text="Load More" callback={loadMoreMovies} />
+            )}
 		</>
 	)
 }
